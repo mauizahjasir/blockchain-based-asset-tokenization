61e47145c56a1bdff0034c8f71fcf8026c6b98ad
Mauizah Nasir <mauizah.nasir@pixeledge.io>
2023-11-11 02:59:22 +0500

    Code commit

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -1,8 +1,8 @@
 <div class="w3-bar-block">
-    <a href="{{ route('home') }}" class="w3-bar-item w3-button w3-padding w3-text-teal{{ Request::is('home') ? ' active' : '' }}"><div style="color:white;">All Users</div></a>
-    <a href="{{ route('new-users') }}" class="w3-bar-item w3-button w3-padding w3-text-teal{{ Request::is('new-users') ? ' active' : '' }}"><div style="color:white;">New Users Request</div></a>
-    <a href="{{ route('get-information') }}" class="w3-bar-item w3-button w3-padding w3-text-teal{{ Request::is('get-information') ? ' active' : '' }}"><div style="color:white;">Get Multichain Information</div></a>
-    <a href="{{ route('create-asset-type') }}" class="w3-bar-item w3-button w3-padding w3-text-teal{{ Request::is('create-asset-type') ? ' active' : '' }}"><div style="color:white;">Create Asset Type</div></a>
-    <a href="{{ route('assets') }}" class="w3-bar-item w3-button w3-padding w3-text-teal{{ Request::is('assets') ? ' active' : '' }}"><div style="color:white;">Assets</div></a>
-    <a href="{{ route('create-asset') }}" class="w3-bar-item w3-button w3-padding w3-text-teal{{ Request::is('create-asset') ? ' active' : '' }}"><div style="color:white;">Create Asset</div></a>
+    <a href="{{ route('home') }}" class="w3-bar-item w3-button w3-padding w3-text-white{{ Request::is('home') ? ' active' : '' }}">All Users</a>
+    <a href="{{ route('new-users') }}" class="w3-bar-item w3-button w3-padding w3-text-white{{ Request::is('new-users') ? ' active' : '' }}">New Users Request</a>
+    <a href="{{ route('get-information') }}" class="w3-bar-item w3-button w3-padding w3-text-white{{ Request::is('get-information') ? ' active' : '' }}">Get Multichain Information</a>
+    <a href="{{ route('create-asset-type') }}" class="w3-bar-item w3-button w3-padding w3-text-white{{ Request::is('create-asset-type') ? ' active' : '' }}">Create Asset Type</a>
+    <a href="{{ route('assets') }}" class="w3-bar-item w3-button w3-padding w3-text-white{{ Request::is('assets') ? ' active' : '' }}">Assets</a>
+    <a href="{{ route('create-asset') }}" class="w3-bar-item w3-button w3-padding w3-text-white{{ Request::is('create-asset') ? ' active' : '' }}">Create Asset</a>
 </div>
